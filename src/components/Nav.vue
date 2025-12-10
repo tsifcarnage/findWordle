914de61b703ecd8fb55726ee42fc0ed8e45c0516
tsifcarnage <59823010+tsifcarnage@users.noreply.github.com>
2025-12-10 21:20:40 +0100

    update

---
+++ src/components/Nav.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: "nav",
+};
+</script>
+<template>
+  <nav>
+    <ul class="bg-fixed flex align-center text-red">
+      <li>Wordle</li>
+    </ul>
+  </nav>
+</template>
+<style></style>
